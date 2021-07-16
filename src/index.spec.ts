44fba47b31a9051177802f125cc1d474484bbbea
Brett Dorrans <hello@lapidist.net>
2021-07-16 19:21:19 +0100

    docs: improve example docs (#14)

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,5 +1,9 @@
-import add from './index';
+import { add, addAll } from './index';
 
 test('it adds two numbers', () => {
     expect(add(1, 1)).toEqual(2);
+});
+
+test('it adds many numbers', () => {
+    expect(addAll(1, 1, 1)).toEqual(3);
 });
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,31 @@
-export default (a: number, b: number): number => a + b;
+/**
+ * Add two numbers
+ *
+ * See:
+ * {@link addAll}
+ *
+ * ```typescript
+ * const sum: number = add(1, 2);
+ * ```
+ * @param a First number
+ * @param b Second number
+ * @returns The sum of both numbers
+ * @category Algebra
+ */
+export const add = (a: number, b: number): number => a + b;
+
+/**
+ * Add many numbers
+ *
+ * See:
+ * {@link addAll}
+ *
+ * ```typescript
+ * const sum: number = addAll(1, 2, 3, 4);
+ * ```
+ * @param numbers Numbers to add
+ * @returns The sum of all numbers
+ * @category Algebra
+ */
+export const addAll = (...numbers: number[]): number =>
+    numbers.reduce((prev, current) => add(prev, current));
